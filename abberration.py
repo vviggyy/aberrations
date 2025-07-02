c444e17c1c3bd8551ff7fe3306549fb8388172c9
Vignesh Vanchinathan <viggy.vanchi@gmail.com>
2025-07-02 16:22:51 -0400

    adding base class

---
+++ abberration.py
@@ -0,0 +1,18 @@
+import pillow
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Abberation:
+    def __init__(self, n: int, m: int):
+        #defines the
+        self.n = n
+        self.m = m
+    
+    def _radial_polynomial():
+        pass
+    
+    def _kernel(w: int, l: int):
+        pass
+    
+    def _convolve(img: PIL.image):
+        pass
